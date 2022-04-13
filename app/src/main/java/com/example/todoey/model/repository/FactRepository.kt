7060644f2c605c54537830164ec4130cae75028b
codewithyadu <80049746+codewithyadu@users.noreply.github.com>
2022-04-13 10:17:06 +0530

    Update FactRepository.kt

--- app/src/main/java/com/example/todoey/model/repository/FactRepository.kt
+++ app/src/main/java/com/example/todoey/model/repository/FactRepository.kt
@@ -10,16 +10,21 @@ class FactRepository {
     suspend fun getFact(): String? {
         if (Internet.hasInternetAccess()) {
             val factApi = RetrofitHelper.getInstance().create(FactApi::class.java)
+            // for string values, either use string resource or create constants
             val res = factApi.getFact("en")
             with(MyApplication.getSharedPrefs().edit()) {
+                // for string values, either use string resource or create constants
                 putString("fact", res.body()?.text)
                 apply()
             }
             return res.body()?.text
         } else {
+            // for string values, either use string resource or create constants
             if (!MyApplication.getSharedPrefs().getString("fact", "").isNullOrEmpty()) {
+                // for string values, either use string resource or create constants
                 return MyApplication.getSharedPrefs().getString("fact", "")
             }
+             // for string values, either use string resource or create constants
             return ""
         }
     }
