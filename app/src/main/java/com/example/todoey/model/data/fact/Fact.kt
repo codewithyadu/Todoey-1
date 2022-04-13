2a5cee6c5a0b5b8d9cb5806950972b64a0d0d454
codewithyadu <80049746+codewithyadu@users.noreply.github.com>
2022-04-13 10:11:20 +0530

    Update Fact.kt

--- app/src/main/java/com/example/todoey/model/data/fact/Fact.kt
+++ app/src/main/java/com/example/todoey/model/data/fact/Fact.kt
@@ -1,5 +1,7 @@
 package com.example.todoey.model.data.fact
 
+
+// Use only val inside data class, using var under data class should be avoided
 data class Fact(
     var id : String,
     var text  :String,
