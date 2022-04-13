ab36bafda1b09c3be28efa32a3e0fef2f79af699
codewithyadu <80049746+codewithyadu@users.noreply.github.com>
2022-04-13 10:13:15 +0530

    Update Internet.kt

--- app/src/main/java/com/example/todoey/model/network/Internet.kt
+++ app/src/main/java/com/example/todoey/model/network/Internet.kt
@@ -9,14 +9,17 @@ import com.example.todoey.MyApplication
 
 object Internet {
     fun hasInternetAccess(): Boolean {
+        // cm ? -> use proper naming conventions 
         val cm = MyApplication.getContext()
             .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // cap ? -> use proper naming conventions 
             val cap = cm.getNetworkCapabilities(cm.activeNetwork) ?: return false
             return cap.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
         } else
             for (n in cm.allNetworks) {
+                 // nInfo ? -> use proper naming conventions 
                 val nInfo = cm.getNetworkInfo(n)
                 if (nInfo != null && nInfo.isConnected) return true
             }
