cbf04a3424c6de68ec9b66334fff9419670613ae
codewithyadu <80049746+codewithyadu@users.noreply.github.com>
2022-04-13 10:14:26 +0530

    Update RetrofitHelper.kt

--- app/src/main/java/com/example/todoey/model/network/RetrofitHelper.kt
+++ app/src/main/java/com/example/todoey/model/network/RetrofitHelper.kt
@@ -5,6 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
 
+    // as it is const so naming convention should be like this : BASE_URL
     private const val baseUrl = "https://uselessfacts.jsph.pl/"
 
     fun getInstance(): Retrofit {
